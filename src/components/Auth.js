29076a403c51c298c23b567a915de32ba07f31f6
akash-panthri <akashpanthri2000@gmail.com>
2024-05-27 04:02:13 +0530

    added Auth.js file and code

---
+++ src/components/Auth.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Auth() {
+  return (
+    <div>
+      <input
+        placeholder="Email..."
+        
+      />
+      <input
+        placeholder="Password..."
+        type="password"
+       
+      />
+      <button> Sign In</button></div>
+  )
+}
