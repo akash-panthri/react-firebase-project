a54e879756c2e3814a44b15fe45e9324d3e776db
akash-panthri <akashpanthri2000@gmail.com>
2024-05-27 04:52:44 +0530

    added signup with google and logout function

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,12 +1,32 @@
 import { useState } from "react";
-import { auth } from "../config/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, googleProvider } from "../config/firebase";
+import { createUserWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 
 export default function Auth() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const signin =async () => {
-    await createUserWithEmailAndPassword(auth, email, password)
+    try {
+        
+        await createUserWithEmailAndPassword(auth, email, password)
+    } catch (error) {
+        console.error(error)
+    }
+  };
+  const signinWithGoogle =async () => {
+    try {
+        
+        await signInWithPopup(auth, googleProvider)
+    } catch (error) {
+        console.error(error)
+    }
+  };
+  const logout = async () => {
+    try {
+      await signOut(auth);
+    } catch (err) {
+      console.error(err);
+    }
   };
   return (
     <div>
@@ -20,6 +40,8 @@ export default function Auth() {
         type="password"
       />
       <button onClick={signin} > Sign In</button>
+      <button onClick={signinWithGoogle}> Sign In With Google</button>
+      <button onClick={logout}> Logout </button>
     </div>
   );
 }
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,7 +2,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import { getAuth,GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,6 +19,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+export const googleProvider = new GoogleAuthProvider();
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 // const analytics = getAnalytics(app);
