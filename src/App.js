5c635f4e008bcc9098421155e4581f94280f42dd
akash-panthri <akashpanthri2000@gmail.com>
2024-06-04 08:35:37 +0530

    Rendered data from db to frontend

--- src/App.js
+++ src/App.js
@@ -3,29 +3,40 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Auth from "./components/Auth.js";
 import { db } from "./config/firebase";
-import {getDocs, collection} from 'firebase/firestore'
+import { getDocs, collection } from "firebase/firestore";
 
 function App() {
   const [movieList, setMovieList] = useState([]);
-  const moviesCollectionRed = collection(db, 'movie')
+  const moviesCollectionRed = collection(db, "movie");
 
-  useEffect(()=>{
-    const getMovieList =async () => {
+  useEffect(() => {
+    const getMovieList = async () => {
       try {
-        const data = await getDocs(moviesCollectionRed)
-        console.log(data)
-        const fileteredData = data.docs.map(doc =>({...doc.data()}))
-        
+        const data = await getDocs(moviesCollectionRed);
+        const fileteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        console.log(fileteredData);
+        setMovieList(fileteredData);
       } catch (error) {
-        console.error(error)
-        
+        console.error(error);
       }
     };
-    getMovieList()
-  },[])
+    getMovieList();
+  }, []);
   return (
     <div className="App">
       <Auth />
+      
+      {movieList.map((movie) => {
+        return (
+          <div>
+            <h1 style={{ color: movie.receivedOscar ? "green" : "red" }}>{movie.title}</h1>{" "}
+            <p> Date: {movie.releaseYear} </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
