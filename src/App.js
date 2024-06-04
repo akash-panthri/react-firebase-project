b0d019b3dcb331e054dd876257e32c1d7c90df9a
akash-panthri <akashpanthri2000@gmail.com>
2024-06-04 08:02:22 +0530

    linked database

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 // import logo from './logo.svg';
-import './App.css';
-import Auth from './components/Auth'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Auth from "./components/Auth.js";
+import { db } from "./config/firebase";
+import {getDocs, collection} from 'firebase/firestore'
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+  const moviesCollectionRed = collection(db, 'movie')
+
+  useEffect(()=>{
+    const getMovieList =async () => {
+      try {
+        const data = await getDocs(moviesCollectionRed)
+        console.log(data)
+        const fileteredData = data.docs.map(doc =>({...doc.data()}))
+        
+      } catch (error) {
+        console.error(error)
+        
+      }
+    };
+    getMovieList()
+  },[])
   return (
     <div className="App">
-     <Auth />
+      <Auth />
     </div>
   );
 }
